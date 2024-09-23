0c82fa2f3bb2d59aa17e4d0f4b3b15b15b894459
celi <celi@Ubuntu.myguest.virtualbox.org>
2024-09-22 22:11:51 -0300

    king tests

--- changelog.md
+++ changelog.md
@@ -6,6 +6,11 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 
+## [0.1.18] - 2024-09-22
+## Added
+
+- king tests
+
 ## [0.1.17] - 2024-09-17
 ### Changed
 
---
+++ tests/test_king.py
@@ -0,0 +1,31 @@
+import unittest
+from game.king import King
+from game.board import Board  
+from game.pawn import Pawn  
+
+
+class TestBishop(unittest.TestCase):
+    def test_move_correct_position(self):
+        board = Board(for_test=True)
+        king = King('WHITE', board)
+        possibles = king.get_possible_positions(4,4)
+        self.assertEqual(possibles, [(5,4),(3,4),(4,5),(4,3),(5,5),(3,5),(5,3),(3,3)])
+    
+    def test_move_other_color_piece(self):
+        board = Board(for_test=True)
+        king = King('WHITE', board)
+        pawn = Pawn('BLACK', board)
+        board.set_piece(5,5, pawn)
+        possibles = king.get_possible_positions(4,4)
+        self.assertEqual(possibles, [(5,4),(3,4),(4,5),(4,3),(5,5),(3,5),(5,3),(3,3)])
+
+    def test_move_same_color_piece(self):
+        board = Board(for_test=True)
+        king = King('WHITE', board)
+        pawn = Pawn('WHITE', board)
+        board.set_piece(5,5, pawn)
+        possibles = king.get_possible_positions(4,4)
+        self.assertEqual(possibles, [(5,4),(3,4),(4,5),(4,3),(3,5),(5,3),(3,3)])
+
+if __name__ == '__main__':
+    unittest.main()
