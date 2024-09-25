8fd6fecaf535577f1e9438295dc200e3bd37ace7
celi <celi@Ubuntu.myguest.virtualbox.org>
2024-09-24 23:36:43 -0300

    fix pawn methods

--- changelog.md
+++ changelog.md
@@ -5,7 +5,17 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.1.18] - 2024-09-22
+## [0.1.20] - 2024-09-24
+### Added
+
+- pawn tests
+
+### Changed
+
+- fixed knight tests
+- fixed duplication on pawn methods
+
+## [0.1.19] - 2024-09-23
 ### Added
 
 - knight tests
--- tests/test_knight.py
+++ tests/test_knight.py
@@ -2,13 +2,51 @@ import unittest
 from game.piece import Piece
 from game.board import Board
 from game.knight import Knight
+from game.pawn import Pawn
 
-class TestPiece(unittest.TestCase):
+class TestKnight(unittest.TestCase):
     def test_any_move(self):
         board = Board(for_test=True)
         knight = Knight('WHITE', board)
-        possibles = knight.get_possible_positions(4,4)
-        self.assertEqual = (possibles, True)
+        possibles = knight.get_possible_positions(4, 4)
+        
+        # Lista de posiciones posibles esperadas
+        expected_positions = [(6, 5), (6, 3), (2, 5), (2, 3), (5, 6), (5, 2), (3, 6), (3, 2)]
+        
+        # Ahora compara la lista de posiciones con la esperada
+        self.assertEqual(possibles, expected_positions)
+
+    def test_any_move_other_piece(self):
+        board = Board(for_test=True)
+        knight = Knight('WHITE', board)
+        pawn = Pawn('BLACK', board)
+        board.set_piece(3, 6, pawn)
+        possibles = knight.get_possible_positions(4, 4)
+        
+        # La lista debería incluir (3, 6) porque el peón es de otro color
+        expected_positions = [(6, 5), (6, 3), (2, 5), (2, 3), (5, 6), (5, 2), (3, 6), (3, 2)]
+        self.assertEqual(possibles, expected_positions)
+
+    def test_any_move_same_piece(self):
+        board = Board(for_test=True)
+        knight = Knight('WHITE', board)
+        pawn = Pawn('WHITE', board)
+        board.set_piece(3, 6, pawn)
+        possibles = knight.get_possible_positions(4, 4)
+        
+        # (3, 6) no debería estar en la lista porque el peón es del mismo color
+        expected_positions = [(6, 5), (6, 3), (2, 5), (2, 3), (5, 6), (5, 2), (3, 2)]
+        self.assertEqual(possibles, expected_positions)
+
+    def test_any_move_jump(self):
+        board = Board(for_test=True)
+        knight = Knight('WHITE', board)
+        pawn = Pawn('WHITE', board)
+        board.set_piece(3, 4, pawn)
+        possibles = knight.get_possible_positions(4, 4)
+
+        expected_positions = [(6, 5), (6, 3), (2, 5), (2, 3), (5, 6), (5, 2),(3,6), (3, 2)]
+        self.assertEqual(possibles, expected_positions)
 
 if __name__ == '__main__':
     unittest.main()
