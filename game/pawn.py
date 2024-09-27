1adc7c98006e611d57c68a9b291ff2dd9d0dca0f
celi <celi@Ubuntu.myguest.virtualbox.org>
2024-09-26 23:07:22 -0300

    pawn test and promotion to queen not completed

--- game/pawn.py
+++ game/pawn.py
@@ -1,4 +1,5 @@
 from game.piece import Piece
+from game.queen import Queen
 
 class Pawn(Piece):
     white_str = '♙'
@@ -23,15 +24,22 @@ class Pawn(Piece):
             other_piece = self.__board__.get_piece(from_row + direction, from_col + col_offset)
             if other_piece and other_piece.__color__ == enemy_color:
                 positions.append((from_row + direction, from_col + col_offset))
+
         return positions
 
     def get_possible_positions_move(self, from_row, from_col):
         direction = 1 if self.__color__ == "BLACK" else -1
         start_row = 1 if self.__color__ == "BLACK" else 6
+        end_row = 7 if self.__color__ == "BLACK" else 0
         positions = []
 
         if self.__board__.get_piece(from_row + direction, from_col) is None:
-            positions.append((from_row + direction, from_col))
+            next_row = from_row + direction
+            positions.append((next_row, from_col))
             if from_row == start_row and self.__board__.get_piece(from_row + 2 * direction, from_col) is None:
                 positions.append((from_row + 2 * direction, from_col))
+            # Promoción a reina
+            if next_row == end_row:
+                self.__board__.set_piece(next_row, from_col, Queen(self.__color__, self.__board__))
+
         return positions
